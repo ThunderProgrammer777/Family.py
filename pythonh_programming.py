7f52170540f4f007576605f0980092fded4f1eaf
ThunderProgrammer777 <shloksoni2007@gmail.com>
2021-06-07 17:51:11 +0530

    nothing

--- pythonh_programming.py
+++ pythonh_programming.py
@@ -12,6 +12,7 @@ def read_member_info():
         info_list = info_list.split(',,')
         return info_list
 
+
 def find(name):
     family_list = read_member_name()
     for family in family_list:
@@ -25,10 +26,12 @@ def find(name):
                     for mem in mem_info:
                         mem = mem.split(",")
                         if mem[0] == name:
-                            print(f"\nName: {mem[0]}.\nAge: {mem[1]}.\nHeight: {mem[2]}.\nWeight: {mem[3]}.\nGender: {mem[4]}.\nBirth Date: {mem[5]}.\nProfession: {mem[6]}")
+                            print(
+                                f"\nName: {mem[0]}.\nAge: {mem[1]}.\nHeight: {mem[2]}.\nWeight: {mem[3]}.\nGender: {mem[4]}.\nBirth Date: {mem[5]}.\nProfession: {mem[6]}")
                         pass
             except:
                 pass
+
 
 def add():
     family_leader = input("Enter the family head name: ")
@@ -41,7 +44,8 @@ def add():
     profession = input("Enter the profession")
 
 
-Choice = ("""f = Find any member information\na = Add member to a particular family\nr = Remove member from a particular family\nq = Quit\nEnter your choice: """)
+Choice = (
+    """f = Find any member information\na = Add member to a particular family\nr = Remove member from a particular family\nq = Quit\nEnter your choice: """)
 choice = str(input(Choice))
 while choice != 'q':
     if choice == 'f':
